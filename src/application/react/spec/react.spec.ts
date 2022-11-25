09748394dce2c87251e6db76efe2475c996f2b1f
BuildTools <unconfigured@null.spigotmc.org>
2022-11-26 03:45:33 +0900

    스펙 todo 작성

---
+++ src/application/react/spec/react.spec.ts
@@ -0,0 +1,7 @@
+import anyTest, { TestFn } from "ava"
+
+const test = anyTest as TestFn
+
+test.todo("키워드를 정확히 입력했을 알맞은 반응과 호감도 반영이 이루어져야 한다.")
+test.todo("동일 키워드의 반응이 여러개 존재할 시 그 중 랜덤으로 반응해야 한다.")
+test.todo("유사 키워드를 정상적으로 인식해야 한다.")
