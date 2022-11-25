019de43579d74b90887e8c12110f924cedbc1551
BuildTools <unconfigured@null.spigotmc.org>
2022-11-26 04:44:09 +0900

    reaction에 키워드 추가

--- src/domain/react/reaction.entity.ts
+++ src/domain/react/reaction.entity.ts
@@ -2,12 +2,14 @@ import { Entity } from "../entity"
 
 export type ReactionProperty = {
   id?: string
+  keyword: string
   response: string
   reward: number
 }
 
 export class Reaction extends Entity {
   public readonly id!: string
+  public readonly keyword!: string
   public readonly response!: string
   public readonly reward!: number
 
--- src/domain/react/reaction.factory.ts
+++ src/domain/react/reaction.factory.ts
@@ -4,6 +4,7 @@ import { Reaction } from "./reaction.entity"
 export const reactionFactory = Factory.define<Reaction>(({ sequence, params }) => {
   const reaction = new Reaction({
     id: `${sequence}`,
+    keyword: `keyword${sequence}`,
     response: `Response of reaction ${sequence}`,
     reward: 1,
   })
